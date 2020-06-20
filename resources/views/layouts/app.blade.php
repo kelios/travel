79f350b21a414ac9bf3006f7c3f71d3ba4113721
kelios_ <ignatieva_julia@tut.by>
2020-06-20 23:58:15 +0300

    fix faveicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="{{ URL::to('/favicon.ico')}}" type="image/x-icon">
 {!! SEO::generate(true) !!}
 <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
