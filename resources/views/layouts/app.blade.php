491f943a7b1b5c24e1ae578f870031aa583914c3
kelios_ <ignatieva_julia@tut.by>
2021-03-19 18:48:28 +0100

    optimize app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <script data-ad-client="ca-pub-9752617241777308" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     @endif
     <!-- Styles -->
-    <link type="text/css" href="{{ mix('/css/metravel.min.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ mix('/css/metravel.min.css') }}" defer rel="stylesheet">
 </head>
 <body>
 <!-- Fonts -->
