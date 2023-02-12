df7f1707d660dfbcfb518c840878c7ff97046d8d
kelios_ <kelios@inbox.ru>
2023-02-12 15:22:58 +0100

    add link to img

--- resources/js/components/TravelCard.vue
+++ resources/js/components/TravelCard.vue
@@ -2,13 +2,15 @@
     <div class="col-mb-11">
 
         <div class="card col-mb-11 box-shadow">
-        <img
-            lazy="loading"
-            :src="travel.travel_image_thumb_url"
-            class="card-img-top"
-            :alt="travel.name"
+            <a :href="'/travels/'+travel.slug" target="_blank">
+                <img
+                    lazy="loading"
+                    :src="travel.travel_image_thumb_url"
+                    class="card-img-top"
+                    :alt="travel.name"
 
-        >
+                >
+            </a>
             <div class="card-body">
 
                 <div class="d-flex justify-content-between align-items-center">
@@ -17,25 +19,27 @@
 
                 <div class="d-flex justify-content-between align-items-center">
                     <p>
-                        <span>{{travel.countryName}}</span>
-                        <span v-if="travel.cityName"> - {{travel.cityName}}</span>
+                        <span>{{ travel.countryName }}</span>
+                        <span v-if="travel.cityName"> - {{ travel.cityName }}</span>
                     </p>
                     <p v-if="!readonly">
-                        <span class="badge badge-success" v-if="travel.publish && travel.moderation">{{__('main.puplish')}}</span>
-                        <span class="badge badge-dark" v-else-if="travel.publish && !travel.moderation">{{__('main.moderationhide')}}</span>
-                        <span class="badge badge-dark" v-else>{{__('main.hide')}}</span>
+                        <span class="badge badge-success"
+                              v-if="travel.publish && travel.moderation">{{ __('main.puplish') }}</span>
+                        <span class="badge badge-dark"
+                              v-else-if="travel.publish && !travel.moderation">{{ __('main.moderationhide') }}</span>
+                        <span class="badge badge-dark" v-else>{{ __('main.hide') }}</span>
                     </p>
                 </div>
-                <div class="small text-right"> {{__('main.author')}} - {{travel.userName}}
+                <div class="small text-right"> {{ __('main.author') }} - {{ travel.userName }}
 
                 </div>
                 <div class="d-flex justify-content-between align-items-center">
                     <p>
                         <a :href="'/travels/'+travel.slug" target="_blank">
-                            {{__('main.readMore')}}
+                            {{ __('main.readMore') }}
                             <i class="fa fa-book" :title="__('main.read')"></i>
                             <span v-if="travel.countUnicIpView">
-                            ({{travel.countUnicIpView}})
+                            ({{ travel.countUnicIpView }})
                             </span>
                         </a>
                     </p>
@@ -44,12 +48,12 @@
                     <div class="btn-group-sm">
                         <button v-if="!readonly" type="button" class="btn btn-sm btn-warning"
                                 v-on:click="goAction({url: travel.url+'/edit' })">
-                            {{__('main.edit')}}
+                            {{ __('main.edit') }}
                         </button>
 
                         <button v-if="!readonly" type="button" class="btn btn-sm btn-danger"
                                 v-on:click="remove(travel.url)">
-                            {{__('main.delete')}}
+                            {{ __('main.delete') }}
                         </button>
                     </div>
                 </div>
@@ -87,27 +91,27 @@
 </template>
 
 <script>
-    export default {
-        name: 'TravelCard',
-        props: ['travel', 'readonly'],
-        created() {
-            // this.data_travel = this.travel;
+export default {
+    name: 'TravelCard',
+    props: ['travel', 'readonly'],
+    created() {
+        // this.data_travel = this.travel;
+    },
+    methods: {
+        goAction: function (data) {
+            window.location.href = data.url;
         },
-        methods: {
-            goAction: function (data) {
-                window.location.href = data.url;
-            },
-            remove: function (url) {
-                let vm = this;
-                axios.delete(url).then(function (response) {
-                    vm.$emit('remove', vm.travel.id);
-                }, function (error) {
-                    console.log(error.response.data.messag);
-                });
-            },
+        remove: function (url) {
+            let vm = this;
+            axios.delete(url).then(function (response) {
+                vm.$emit('remove', vm.travel.id);
+            }, function (error) {
+                console.log(error.response.data.messag);
+            });
+        },
 
-        },
-    }
+    },
+}
 </script>
 
 <style scoped>
