ea9767718185d73969f01ae1922db489fa0fcc08
kelios_ <kelios@inbox.ru>
2023-02-25 22:10:47 +0100

    fix webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -21,8 +21,8 @@ mix.webpackConfig({
 mix.js('resources/js/app.js', 'public/js')
     .js(["resources/js/admin/admin.js"], "public/js")
     .vue({ version: 2 })
-    .postCss('public/css/metravel.css',  'public/css')
-    .postCss('public/css/showmetravel.css', 'public/css')
+    .postCss('public/css/metravel.css',  'public/css/metravel.min.css')
+    .postCss('public/css/showmetravel.css', 'public/css/showmetravel.min.css')
     .sass("resources/sass/admin/admin.scss", "public/css")
     .purgeCss({
         enabled: true,
